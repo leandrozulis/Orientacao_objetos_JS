de05900cbe469ed3ee320d6ecdf970c273aa7ae1
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-05-02 11:19:30 -0300

    Introdução sobre heranças

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import {Cliente} from './Cliente.js';
 import {ContaCorrente} from './ContaCorrente.js';
+import {ContaPoupanca} from './ContaPoupanca.js';
+import {Conta} from './Conta.js';
 
 const cliente1 = new Cliente('Leandro',11122233309);
-const cliente2 = new Cliente('Alice', 44455566609);
 
-const contaCorrenteLeandro = new ContaCorrente(1001, cliente1);
+const contaCorrenteLeandro = new Conta(0, cliente1, 1001);
 contaCorrenteLeandro.depositar(500);
 
-const conta2 = new ContaCorrente(102, cliente2);
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-contaCorrenteLeandro.transferir(200, conta2);
-
-console.log(ContaCorrente.numeroDeConstas);
+console.log(contaPoupanca);
+console.log(contaCorrenteLeandro);
