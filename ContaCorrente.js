d282d1e6675f8ddfc2ed6d697c956cef86836649
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-05-08 11:27:29 -0300

    Comentando sobre super e inicio de abstração

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -7,4 +7,13 @@ export class ContaCorrente extends Conta{
         super(0, cliente, agencia);
         ContaCorrente.numeroDeConstas++;
     }
+
+    sacar(valor) {
+        let taxa = 1.1;
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+    }
 }
--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const cliente1 = new Cliente('Leandro',11122233309);
 
 const contaCorrenteLeandro = new ContaCorrente(cliente1, 1001);
 contaCorrenteLeandro.depositar(500);
+contaCorrenteLeandro.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
 console.log(contaCorrenteLeandro);
 console.log(contaPoupanca);
