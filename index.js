43af8c30a81089f417c3ae6cc69ddef27777d965
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-05-12 16:47:46 -0300

    Polimorfismo

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ import {Cliente} from './Cliente.js';
 import {ContaCorrente} from './Conta/ContaCorrente.js';
 import {ContaPoupanca} from './Conta/ContaPoupanca.js';
 import { ContaSalario } from './Conta/ContaSalario.js';
+import { Gerente } from './Funcionarios/Gerente.js';
+import { Diretor } from './Funcionarios/Diretor.js';
+import { SistemaAutenticacao } from './SistemaAutenticacao.js';
 
-const cliente1 = new Cliente('Leandro',11122233309);
+// const cliente1 = new Cliente('Leandro',11122233309);
+// const contaCorrenteLeandro = new ContaCorrente(cliente1, 1001);
+// const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+// const contaSalario = new ContaSalario(cliente1);
+// contaSalario.depositar(100);
+// contaSalario.sacar(10);
+// console.log(contaSalario);
 
-const contaCorrenteLeandro = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const diretor = new Diretor('Leandro', 10000, 12345678900);
+diretor.cadastrarSenha("123456789");
 
-console.log(contaSalario);
+const gerente = new Gerente('Lele', 5000, 12365498701);
+gerente.cadastrarSenha("123456789");
+
+const estalogado = SistemaAutenticacao.login(gerente, "123456789");
+
+console.log(estalogado);
